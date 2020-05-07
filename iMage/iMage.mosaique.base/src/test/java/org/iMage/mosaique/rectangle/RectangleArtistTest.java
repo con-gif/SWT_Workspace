2e7e46ce40dd7be3fcc633edd8f38b4d317da705
Kaloyan Draganov <KaloyanDDraganov@outlook.com>
2020-05-07 17:27:35 +0200

    Test class created for RectangleArtist class in Maven respective directory

---
+++ iMage/iMage.mosaique.base/src/test/java/org/iMage/mosaique/rectangle/RectangleArtistTest.java
@@ -0,0 +1,15 @@
+package org.iMage.mosaique.rectangle;
+
+import org.junit.*;
+
+public class RectangleArtistTest {
+
+    private RectangleShape rectangleShape;
+
+    @Before
+    public void test() {
+
+    }
+
+
+}
