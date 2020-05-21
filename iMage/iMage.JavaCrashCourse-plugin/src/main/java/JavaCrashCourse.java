2a9e4d94806815bed540c34a7dd97de798c2fafd
Kaloyan Draganov <KaloyanDDraganov@outlook.com>
2020-05-21 11:24:23 +0200

    Implemented init() method of new jmjrst plugin.

--- iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
+++ iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
@@ -1,9 +1,25 @@
 import org.iMage.plugins.PluginForJmjrst;
 import org.jis.Main;
 
+import java.util.List;
+
 public class JavaCrashCourse extends PluginForJmjrst {
 
     private static final String NAME = "Java Crash Course";
+    private static final int NUMBER_OF_ATTRIBUTES = 1;
+
+    /**
+     * Source: Oracle Corp.
+     */
+    private static final String JAVA_8_LTS = "Java 8 (LTS)";
+    private static final String JAVA_9 = "Java 9 (non‑LTS)";
+    private static final String JAVA_10 = "Java 10 (non‑LTS)";
+    private static final String JAVA_11_LTS = "Java 11 (LTS)";
+    private static final String JAVA_12 = "Java 12 (non‑LTS)";
+    private static final String JAVA_13 = "Java 13 (non‑LTS)";
+    private static final String JAVA_14 = "Java 14 (non‑LTS)";
+
+    private List<String> javaStableReleases;
 
     @Override
     public String getName() {
@@ -12,12 +28,12 @@ public class JavaCrashCourse extends PluginForJmjrst {
 
     @Override
     public int getNumberOfParameters() {
-        return 0;
+        return NUMBER_OF_ATTRIBUTES;
     }
 
     @Override
     public void init(Main main) {
-
+        javaStableReleases = List.of(JAVA_8_LTS, JAVA_9, JAVA_10, JAVA_11_LTS, JAVA_12, JAVA_13, JAVA_14);
     }
 
     @Override
