ba12f8d42c91e1af70faad0b34291a76f424f9fd
Kaloyan Draganov <KaloyanDDraganov@outlook.com>
2020-05-21 09:22:45 +0200

    Implemented getPlugins() method in class PluginManagement.

--- iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
+++ iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginManagement.java
@@ -1,11 +1,19 @@
 package org.iMage.plugins;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ServiceLoader;
+
 /**
  * Knows all available plug-ins and is responsible for using the service loader API to detect them.
  *
  * @author Dominik Fuchss
  */
 public final class PluginManagement {
+
+  private static ServiceLoader<PluginForJmjrst> loader;
 
   /**
    * No constructor for utility class.
@@ -22,8 +30,17 @@ public final class PluginManagement {
    * @return an {@link Iterable} Object containing all available plug-ins
    */
   public static Iterable<PluginForJmjrst> getPlugins() {
-    //TODO: implement me!
-    return null;
+
+    // fetching all plugins
+    loader = ServiceLoader.load(PluginForJmjrst.class);
+    Iterator<PluginForJmjrst> pluginIterator = loader.iterator();
+    List<PluginForJmjrst> plugins = new ArrayList<>();
+
+    while (pluginIterator.hasNext()) {
+      plugins.add(pluginIterator.next());
+    }
+    Collections.sort(plugins);
+    return plugins;
   }
 
 }
