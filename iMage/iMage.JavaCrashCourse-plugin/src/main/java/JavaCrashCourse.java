5a6375d084499c671c2718e7ec4e65cf4054303f
Kaloyan Draganov <KaloyanDDraganov@outlook.com>
2020-05-21 10:56:26 +0200

    Created a plugin for the java crash course.

---
+++ iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
@@ -0,0 +1,37 @@
+import org.iMage.plugins.PluginForJmjrst;
+import org.jis.Main;
+
+public class JavaCrashCourse extends PluginForJmjrst {
+
+    private static final String NAME = "Java Crash Course";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int getNumberOfParameters() {
+        return 0;
+    }
+
+    @Override
+    public void init(Main main) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public void configure() {
+
+    }
+}
