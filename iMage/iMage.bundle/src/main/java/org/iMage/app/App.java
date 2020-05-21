86c25189ab07df06e7acdd594a9a734973e8b4ab
Kaloyan Draganov <KaloyanDDraganov@outlook.com>
2020-05-21 14:54:51 +0200

    Created an Application class in module Bundle as a gateway to jmjrst with plugin support.

---
+++ iMage/iMage.bundle/src/main/java/org/iMage/app/App.java
@@ -0,0 +1,19 @@
+package org.iMage.app;
+
+import org.jis.Main;
+
+/**
+ * iMage Application.
+ *
+ * @author Kaloyan Draganov
+ */
+public class App {
+
+    /**
+     * Runs Jmjrst.main
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
